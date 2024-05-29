7e599018cf07ce30901b41d4f21a38e60c9478c2
Joao Reinert <joao33reinert@gmail.com>
2024-05-29 10:16:45 -0300

    atualizando os imports

--- lib/appWidget.dart
+++ lib/appWidget.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jogos/jogoDaForca.dart';
-import 'package:jogos/jogoDaVelha.dart';
 import 'package:jogos/jogoDoTermo.dart';
-import 'package:provider/provider.dart';
 
 class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
